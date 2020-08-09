cdeca2dd4c112117c82706bc229b9195577d7bc3
Venice Vargas <venicevargas1@live.com>
2020-08-09 03:30:54 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,15 +1,13 @@
   
-def hello_t(array)
-    i = 0
-    while i < array.length
-      yield(array[i])
-      i = i + 1
-    end
-    array
-end
+require 'pry'
 
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+def hello(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += 1
   end
 end
+
+
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
